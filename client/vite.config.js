34aafc3f6b9586c5abecd4ca5a948e14301e189a
cristiano-Senai <cristiano.s.santos@ba.estudante.senai.br>
2026-04-25 10:19:04 -0300

    chore(client): split exceljs into vendor chunk (vite config)

--- client/vite.config.js
+++ client/vite.config.js
@@ -48,7 +48,7 @@ export default defineConfig(({ command, mode }) => {
       rollupOptions: {
         output: {
           manualChunks: {
-            vendor: ['react', 'react-dom', 'react-router-dom'],
+            vendor: ['react', 'react-dom', 'react-router-dom', 'exceljs'],
             ui: ['lucide-react', 'react-hot-toast'],
           },
         },
